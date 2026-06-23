5dede913cf29f1af17ccd3caf0a0ab9bd8a19304
Fatemeyari <fatemeyarii82@gmail.com>
2026-06-23 15:04:01 +0330

    add verification token generation logic

--- core/accounts/tokens.py
+++ core/accounts/tokens.py
@@ -2,7 +2,14 @@ import secrets
 from datetime import timedelta
 
 from django.utils import timezone
-from .models import PasswordResetToken
+from .models import PasswordResetToken , VerificationToken
+
+
+
+def invalidate_user_token(user):
+    PasswordResetToken.objects.filter(user=user , is_used=False).update(is_used=True)
+
+
 
 def create_token(user):
     invalidate_user_token(user)
@@ -17,5 +24,20 @@ def create_token(user):
     return token
 
 
-def invalidate_user_token(user):
-    PasswordResetToken.objects.filter(user=user , is_used=False).update(is_used=True)
+
+
+def invalidate_user_verification_token(user):
+    VerificationToken.objects.filter(user=user , is_used=False).update(is_used=True)
+
+
+def create_verification_token(user):
+    invalidate_user_verification_token(user)
+    token=secrets.token_urlsafe(32)
+    expires_time=timezone.now() + timedelta(hours=24)
+    VerificationToken.objects.create(
+        user=user,
+        token=token,
+        is_used=False,
+        expires_time=expires_time
+    )
+    return token
