8a6ce060f415c5ee244af3d2391831bc7c6d89b2
Fatemeyari <fatemeyarii82@gmail.com>
2026-06-23 15:36:31 +0330

    add verification email celery task

--- core/accounts/emails.py
+++ core/accounts/emails.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from .tokens import create_token
+from .tokens import create_token , create_verification_token
 from .models import User
 
 
@@ -18,7 +18,7 @@ def send_email(user_id):
 
     send_mail(
         subject="Reset Password Your Account",
-        message=f"fClick this link to reset your password:\n{token_link}",
+        message=f"Click this link to reset your password:\n{token_link}",
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[user.email],
         fail_silently=False
@@ -26,3 +26,16 @@ def send_email(user_id):
     )
 
 
+@shared_task
+def send_verification_email(user_id):
+    user=User.objects.get(id=user_id)
+    token=create_verification_token(user)
+    token_link=(f"http://127.0.0.1:8000/accounts/signup-confirm/{token}")
+    send_mail(
+        subject="Verification Your Account",
+        message=f"Click this link to verify your account:\n{token_link}",
+
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user.email],
+        fail_silently=False
+    )
