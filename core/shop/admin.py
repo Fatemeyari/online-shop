1513e24aaed0ddf2c27608b8364fa6ac43dd2318
Fatemeyari <fatemeyarii82@gmail.com>
2026-06-24 17:13:58 +0330

    Register shop models in Django admin

--- core/shop/admin.py
+++ core/shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProductCategory , Product , ProductImage , WishlistProduct 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=("id" , "title" ,"stock" ,"status" , "price" , "discount_percent" , "created_time")
+    search_fields=("title" ,)
+    list_filter=("status" , "created_time")
+
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display=("id" , "title" , "created_time")
+    search_fields=("title",)
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display=("id" , "product","file" ,"created_time")
+
+@admin.register(WishlistProduct)
+class WishlistProductAdmin(admin.ModelAdmin):
+    list_display=("id" , "user" ,"product")
