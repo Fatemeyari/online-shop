e817c67b44bd989efccdbf06a86def9024f6040c
Fatemeyari <fatemeyarii82@gmail.com>
2026-06-24 16:17:26 +0330

    Add product category model

--- core/shop/models.py
+++ core/shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from decimal import Decimal 
+from django.core.validators import MaxValueValidator , MinValueValidator
 
-# Create your models here.
+
+class ProductStatusType(models.IntegerChoices):
+    publish=1 , ("نمایش")
+    draft=2 , ("َعدم نمایش")
+
+class ProductCategory(models.Model):
+    title=models.CharField(max_length=255)
+    slug=models.SlugField(allow_unicode=True ,unique=True)
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=["-created_time"]
+    
+    def __str__(self):
+        return self.title
+
+
