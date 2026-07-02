90d97f587c88112ec2de5bbc114643c429d0a7e1
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-02 16:38:03 +0330

    add similar products feature

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -10,6 +10,13 @@ def show_latest_products():
         status = ProductStatusType.publish.value).order_by("-created_time")[:8]
     return {"latest_products": latest_products}
 
+@register.inclusion_tag("includes/similar_products.html")
+def show_similar_products(product):
+    product_categories=product.category.all()
+    similar_products=Product.objects.filter(
+        status = ProductStatusType.publish.value , category__in=product_categories).distinct().order_by("-created_time")[:4]
+    return {"similar_products": similar_products}
+
 @register.inclusion_tag("includes/show_categories.html")
 def show_categories():
     categories=ProductCategory.objects.all()[:4]
