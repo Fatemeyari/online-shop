282f5451c267d15caaed55e8452f90856ee4e9e9
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-03 16:11:17 +0330

    add contact and newsletter admin configuration

--- core/website/admin.py
+++ core/website/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactUs , NewsLetter
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display=("fullname" , "email" ,"phone_number" , "is_seen", "created_time")
+    search_fields = ("fullname", "email", "phone_number")
+    list_filter = ("is_seen", "created_time")
+    
+@admin.register(NewsLetter)
+class NewsLetterAdmin(admin.ModelAdmin):
+    list_display=( "email", "created_time")
+    search_fields=("email",)
+
+
+
+
+
+
+
