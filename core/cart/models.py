b8642cc751a708f31aa0e38bfcd310ed2ca5690c
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-04 14:50:13 +0330

    create cart app and add cart models

---
+++ core/cart/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class CartModel(models.Model):
+    user=models.ForeignKey('accounts.User',on_delete=models.CASCADE)
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering=['-created_time']
+        verbose_name='Cart'
+        verbose_name_plural='Carts'
+
+    
+    def __str__(self):
+        return self.user.phone_number
+
+class CartItemModel(models.Model):
+    cart = models.ForeignKey(CartModel ,on_delete=models.CASCADE)
+    product=models.ForeignKey('shop.Product' , on_delete=models.PROTECT)
+    quantity=models.PositiveIntegerField(default=0)
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['-created_time']
+        verbose_name='Cart Item'
+        verbose_name_plural='Cart Items'
+
+    def __str__(self):
+        return self.cart
+
+
