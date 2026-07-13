6abfd1ebba17cc16ad15a1b15f2850e91989dc22
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-13 16:23:35 +0330

    set up dashboard urls and redirects

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.urls import reverse_lazy
+from django.views.generic import View 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from accounts.models import UserType
+
+class DashboardHomeView(LoginRequiredMixin,View):
+    def dispatch(self , request , *args , **kwargs):
+        if request.user.is_authenticated:
+            if request.user.type == UserType.customer.value:
+                return redirect(reverse_lazy('#'))
+            elif request.user.type == UserType.admin.value:
+                return redirect(reverse_lazy('#'))
+        else:
+            return redirect(reverse_lazy('accounts:login'))
+
+        return super().dispatch(request , *args , **kwargs)
+
