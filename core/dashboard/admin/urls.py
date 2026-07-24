d7a7c0a9481a248ccb3c505fb9975a3b1ca2d241
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-24 15:02:34 +0330

    add admin product list with filters

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -7,6 +7,8 @@ app_name ='admin'
 urlpatterns = [
    path('home/' , views.AdminDashboardHomeView.as_view() , name="home"),
    path('security-edit/' , views.AdminSecurityEditView.as_view() , name="security-edit"),
-   path('profile-edit/' , views.AdminProfileEditView.as_view() , name="profile-edit")
+   path('profile-edit/' , views.AdminProfileEditView.as_view() , name="profile-edit"),
+   path('product-list/' , views.AdminProductListView.as_view() , name="product-list")
+
   
 ]
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from django.urls import reverse_lazy
-from django.views.generic import View , TemplateView , UpdateView
+from django.views.generic import View , TemplateView , UpdateView , ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
 from django.contrib.messages.views import SuccessMessageMixin 
@@ -8,7 +8,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from dashboard.permissions import HasAdminAccessPermission
 from accounts.models import User , Profile
 from dashboard.admin.forms import AdminPasswordChangeForm ,AdminProfileEditForm
-
+from shop.models import Product,ProductCategory,ProductStatusType
 class AdminDashboardHomeView(LoginRequiredMixin,HasAdminAccessPermission,TemplateView):
     template_name="dashboard/admin/home.html"
 
@@ -32,6 +32,59 @@ class AdminProfileEditView(LoginRequiredMixin,HasAdminAccessPermission,SuccessMe
     def get_object(self, queryset=None):
         return Profile.objects.get(user=self.request.user)
 
+
+
+
+class AdminProductListView(LoginRequiredMixin,HasAdminAccessPermission,ListView):
+    template_name="dashboard/admin/product_list.html"
+    paginate_by = 9
+
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(
+                title__icontains=search_q
+            )
+
+        if category := self.request.GET.get("category"):
+            queryset = queryset.filter(
+                category__id=category
+            )
+
+        if min_price := self.request.GET.get("min_price"):
+            queryset = queryset.filter(
+                price__gte=min_price
+            )
+
+        if max_price := self.request.GET.get("max_price"):
+            queryset = queryset.filter(
+                price__lte=max_price
+            )
+
+        if sort := self.request.GET.get("sort"):
+
+            if sort == "newest":
+                queryset = queryset.order_by("-created_time")
+
+            elif sort == "oldest":
+                queryset = queryset.order_by("created_time")
+
+            elif sort == "cheap":
+                queryset = queryset.order_by("price")
+
+            elif sort == "expensive":
+                queryset = queryset.order_by("-price")
+
+        return queryset.distinct()
+                
+
+    def get_context_data(self , **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["total_items"] =self.get_queryset().count()
+        context["categories"]= ProductCategory.objects.all()
+        return context  
 
 
 
