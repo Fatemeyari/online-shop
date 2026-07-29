4b3bb5ed20c9758b18845542f463951d33d641c5
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-29 15:34:17 +0330

    add cart admin configuration

--- core/cart/admin.py
+++ core/cart/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CartModel , CartItemModel
+
+@admin.register(CartModel)
+class CartAdmin(admin.ModelAdmin):
+    fields = ("user",)
+    search_fields=("user" ,)
+    list_filter=("user__email",)
+
+@admin.register(CartItemModel)
+class CartItemAdmin(admin.ModelAdmin):
+    fields = ("cart" , "product" ,"quantity")
+    search_fields=("cart__user__email" , "product__title")
+    list_filter=("cart","product")
