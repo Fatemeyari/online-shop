999d0096215157132e535de1044c2be88ebc7ce0
Fatemeyari <fatemeyarii82@gmail.com>
2026-07-30 18:30:16 +0330

    Configure Django admin for order models

--- core/order/admin.py
+++ core/order/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CouponModel , UserAddressModel, OrderModel , OrderItemModel
+
+@admin.register(OrderModel)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "user" ,
+        "total_price" ,
+        "status" ,
+        "coupon" ,
+        "updated_time",
+        "created_time"
+        )
+
+    search_fields=("user__phone_number", )
+    list_filter=("user" , "status")
+
+
+@admin.register(OrderItemModel)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "order" ,
+        "product" ,
+        "quantity" ,
+        "price" ,
+        "updated_time",
+        "created_time"
+        )
+
+    search_fields=("order_user__phone_number", )
+    list_filter=("product" ,)
+
+
+
+
+@admin.register(CouponModel)
+class CouponmAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "code" ,
+        "discount_percent" ,
+        "max_limit_usage" ,
+        "used_by_count" ,
+        "expiration_date",
+        "updated_time",
+        "created_time"
+        )
+    def used_by_count(self , obj):
+        return obj.used_by.all().count()
+    used_by_count.short_description = "Users"
+
+
+
+@admin.register(UserAddressModel)
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "user" ,
+        "address" ,
+        "state" ,
+        "city" ,
+        "zip_code",
+        "updated_time",
+        "created_time"
+        )
+
+    search_fields=("user__phone_number",)
+    list_filter=("user" ,"state" , "city")
+
