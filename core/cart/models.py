cf237dbf54f5aa2a82fa1dd5894d49a320be391c
Fatemeyari <fatemeyarii82@gmail.com>
2026-08-01 19:35:15 +0330

    add cart calculation methods

--- core/cart/models.py
+++ core/cart/models.py
@@ -17,8 +17,18 @@ class CartModel(models.Model):
     def __str__(self):
         return self.user.email
 
+    def calculate_total_price(self):
+        return sum(item.product.get_price() * item.quantity for item in self.cart_items.all())
+
+    def calculate_total_discount(self):
+        return sum(item.product.get_discount() * item.quantity for item in self.cart_items.all())
+    
+
+    def calculate_total_quantity(self):
+        return sum(item.quantity for item in self.cart_items.all())
+
 class CartItemModel(models.Model):
-    cart = models.ForeignKey(CartModel ,on_delete=models.CASCADE)
+    cart = models.ForeignKey(CartModel ,on_delete=models.CASCADE, related_name="cart_items")
     product=models.ForeignKey('shop.Product' , on_delete=models.PROTECT)
     quantity=models.PositiveIntegerField(default=0)
     created_time=models.DateTimeField(auto_now_add=True)
