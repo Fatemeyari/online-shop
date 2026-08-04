f4d229706087e8ccade7c6dc65454f63e99d22d8
Fatemeyari <fatemeyarii82@gmail.com>
2026-08-04 18:36:32 +0330

    add clear method to cart

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -116,7 +116,9 @@ class CartSession:
         session_product_ids= [item["product_id"] for item in self._cart["items"]]   
         CartItemModel.objects.filter(cart=cart).exclude(product__id__in=session_product_ids).delete()
 
-
+    def clear(self):
+        self._cart = self.session["cart"] = {"items": []}
+        self.save()
 
     def save(self):
         self.session.modified=True
