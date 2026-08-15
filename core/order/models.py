95c428871a353b1bc1274458c52869c814e829a1
Fatemeyari <fatemeyarii82@gmail.com>
2026-08-15 18:14:24 +0330

    update order model

--- core/order/models.py
+++ core/order/models.py
@@ -51,8 +51,8 @@ class UserAddressModel(models.Model):
 
 class OrderModel(models.Model):
     user = models.ForeignKey('accounts.User' , on_delete=models.PROTECT, related_name="orders")
-    payment = models.ForeignKey('payment.PayMentModel' , on_delete=models.SET_NULL,null=True , blank=True )
-    address=models.ForeignKey(UserAddressModel , on_delete=models.CASCADE, related_name="orders")
+    payment = models.ForeignKey('payment.PayMentModel' , on_delete=models.SET_NULL,null=True , blank=True , related_name="payment_order" )
+    address=models.ForeignKey(UserAddressModel , on_delete=models.CASCADE, related_name="address_order")
     total_price=models.DecimalField(default=0 , max_digits=10 , decimal_places=0)
     status=models.IntegerField(choices=OrderStatusType.choices, default=OrderStatusType.pending.value)
     coupon=models.ForeignKey(CouponModel, on_delete=models.PROTECT , null=True , blank=True)
