574e818536ef7d2f4eba75deb169a6115acd45a1
Fatemeyari <fatemeyarii82@gmail.com>
2026-08-25 18:09:35 +0330

    Add wishlist support to product template tags

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -1,21 +1,30 @@
 from django import template 
 
-from shop.models import ProductStatusType , Product ,ProductCategory
+from shop.models import ProductStatusType , Product ,ProductCategory ,WishlistProduct
 
 register = template.Library()
 
-@register.inclusion_tag("includes/latest_products.html")
-def show_latest_products():
-    latest_products=Product.objects.filter(
-        status = ProductStatusType.publish.value).order_by("-created_time")[:8]
-    return {"latest_products": latest_products}
+@register.inclusion_tag("includes/latest_products.html",takes_context=True)
+def show_latest_products(context):
+    request = context["request"]
+    latest_products=Product.objects.filter(status = ProductStatusType.publish.value).order_by("-created_time")[:8]
+    if request.user.is_authenticated:
+        wish_items = WishlistProduct.objects.filter(user=request.user).values_list("product_id",flat=True)
+    else:
+        wish_items = []
+    return {"latest_products": latest_products,"wish_items": wish_items,}
 
-@register.inclusion_tag("includes/similar_products.html")
-def show_similar_products(product):
+@register.inclusion_tag("includes/similar_products.html",takes_context=True )
+def show_similar_products(context,product):
+    request = context["request"]
     product_categories=product.category.all()
     similar_products=Product.objects.filter(
         status = ProductStatusType.publish.value , category__in=product_categories).distinct().order_by("-created_time")[:4]
-    return {"similar_products": similar_products}
+    if request.user.is_authenticated:
+        wish_items = WishlistProduct.objects.filter(user=request.user).values_list("product_id",flat=True)
+    else:
+        wish_items = []
+    return {"similar_products": similar_products,"wish_items": wish_items,}
 
 @register.inclusion_tag("includes/show_categories.html")
 def show_categories():
