fa722ec960ad76dbee6c920a32f0b0f74203db41
Fatemeyari <fatemeyarii82@gmail.com>
2026-08-29 18:45:43 +0330

    add ReviewModelAdmin

--- core/review/admin.py
+++ core/review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import ReviewModel
 # Register your models here.
+
+
+@admin.register(ReviewModel)
+class ReviewModelModelAdmin(admin.ModelAdmin):
+    list_display =("id","user","product","rate","created_time")
